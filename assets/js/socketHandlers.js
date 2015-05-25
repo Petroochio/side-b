a2175745b9085856409c02f1978bcb7726b7064b
Peter Gyory <ptg3662@rit.edu>
2015-05-25 19:23:38 -0400

    Add local files

---
+++ assets/js/socketHandlers.js
@@ -0,0 +1,107 @@
+"use strict";
+var game = game || {};
+
+game.socketHandlers = {
+  socket : undefined,
+  room: "",
+  init : function(app) {
+    //setting client's own properties (MIGHT NOT BE THE BEST PRACTICE);
+    var socket = io.connect( window.location.origin, {query: 'user='+name, type: 'desktop'});
+    this.socket = socket;
+    var self = this;
+    var room = this.room;
+    var password = "";
+    var connectData = {};
+    var me = this;
+    socket.emit('hostConnect', connectData);//BEFORE ROOM
+    socket.on('hostEstablish', function(data){
+      console.log("ROOM: " + data.room);
+      room = data.room;
+      self.room = data.room;
+      app.room = data.room;
+    });
+    //Set up socket events --Ha Andrew don't look at this --You can't stop me
+    socket.on('player join', function(data){
+      if(app.state === "START") {
+        var x = app.canvas.width/2, y = app.canvas.height/2;
+        app.players[data.id] = new game.Newbie(data.id, data.color, x, y, data.name);
+        app.playerIDs.push(data.id);
+        console.log(app.playerIDs);
+      }
+    });
+
+    socket.on('player leave', function(data){
+      if(app.players[data.id])
+        app.playerIDs.splice(app.playerIDs.indexOf(data.id),1);
+      delete app.players[data.id];
+    });
+
+    socket.on('select class', function(data){
+      if(app.players[data.id]){
+        var x = app.players[data.id].x, y = app.players[data.id].y;
+        var name = app.players[data.id].name;
+        delete app.players[data.id];
+        var newClass = undefined;
+        
+        switch(data.class) {
+          case 'speed':
+            newClass = new game.Speed(data.id, data.color, x, y, name);
+            break;
+          case 'matador':
+            newClass = new game.Matador(data.id, data.color, x, y, name);
+            break;
+          case 'newbie':
+            newClass = new game.Newbie(data.id, data.color, x, y, name);
+            break;
+          case 'ghost':
+            newClass = new game.Ghost(data.id, data.color, x, y, name);
+            break;
+        }
+        newClass.ready = true;
+        app.players[data.id] = newClass;
+      }
+
+    });
+
+    socket.on('player ready', function(data){
+      console.log(data.id + " ready");
+      if(app.players[data.id].ready)
+        app.players[data.id].ready = false;
+        else {
+            app.players[data.id].ready = true;
+            app.readySound.play();
+        }
+    });
+
+    socket.on('charge start', function(data){
+      app.players[data.id].beginCharge(data.type);
+        app.chargeSound.play();
+    });
+
+    socket.on('charge end', function(data){
+      app.players[data.id].endCharge(data.type);
+        app.chargeSound.pause();
+        app.chargeSound.currentTime = 0;
+    });
+
+    socket.on('phone tilt', function(data) {
+      if(app.players[data.id])
+        app.players[data.id].updateAcceleration(data.yAcc/300, -data.xAcc/300);
+      //my eyes are everywhere --I will gouge your eyes out
+    });
+    //use for sending data between different views
+    socket.on('getSnapshot', function(data){
+      me.sendSnapshot(data);
+    });
+    app.loop();
+  },
+
+  changeState : function(newState) {
+    var data = {state : newState, room: this.room};
+    this.socket.emit('state change', data);
+  },
+  //sends snapshot to server to update other views with
+  sendSnapshot : function(data) {
+
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var path = require('path');
+var io = require('socket.io')(http);
+var MobileDetect = require('mobile-detect');
+//var mongoose = require('mongoose'); 
+var users = {};
+var rooms = {};
+var port = process.env.PORT || process.env.NODE_PORT || 3000;
+//models
+/*var models = require('./models');
+var Account = models.Account;*/
+
+var dbURL = process.env.MONGOLAB_URI || "mongodb://localhost/BirdMaker";
+/*
+var db = mongoose.connect(dbURL, function(err) {
+    if(err) {
+        console.log("Could not connect to database");
+        throw err;
+    }
+});*/
+//Function for generating a random room key
+var generateRoomKey = function(){
+  var pw = "";
+  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXTZABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var string_length = 4;
+  for (var i=0; i<string_length; i++) {
+    var rnum = Math.floor(Math.random() * chars.length);
+    pw += chars.substring(rnum,rnum+1);
+  }
+  return pw;
+}
+
+app.use('/assets', express.static(path.resolve(__dirname) + '../../assets'));
+app.get('/', function(req, res){
+  //the html string being sent
+  var md = new MobileDetect(req.headers['user-agent']);
+  var filepath = path.resolve(__dirname + '/../assets/views/index.html');
+  if(md.mobile() != null) {
+    filepath = path.resolve(__dirname + '/../assets/views/mobile_index.html');
+  }
+  res.sendFile(filepath);
+});
+// **** ACTIONS DONE BY SERVER HAVE HIGHER PRIORITY AND INIT BEFORE CLIENT ACTIONS
+
+// Setup socket.io
+// listen to connection
+io.on('connection', function(socket){
+  //broadcast that a user has connected
+  //pass an object containing user informatiojn?
+  //Create a new room to host the game
+
+  //////////////////
+  //ACCOUNT EVENTS
+  //////////////////
+  socket.on('player join', function(data){
+    data.id = socket.id;
+    // check if total players have maxed
+    io.emit('player join', data);
+    io.to(socket.id).emit('player id', socket.id);
+  });
+  
+  //////////////////
+  //GAME EVENTS
+  //////////////////
+  socket.on('sling_fire', function(data){
+    data.id = socket.id;
+    io.emit('sling_fire', data);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:3000');
+});
