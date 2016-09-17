0781a8a642be0fd97e1a3bf947d4d0c27683334b
Peter Gyory <ptg3662@rit.edu>
2016-09-17 18:07:36 -0400

    Draw aiming vector

--- src/client/renderers/player.js
+++ src/client/renderers/player.js
@@ -1,16 +1,32 @@
 import { curry } from 'ramda';
 
-const renderPlayer = ( ctx, player ) => {
-  console.log( player.launch );
-  const [ px, py ] = player.position.value;
+const drawAimVector = ( ctx, { position, launch } ) => {
+  if ( launch.mag() != 0 ) {
+    const [ px, py ] = position.value;
+    const [ aimX, aimY ] = launch.value;
+    const inverseY = window.innerHeight - py;
+
+    ctx.beginPath();
+    ctx.moveTo( px, inverseY );
+    ctx.lineTo( px + aimX, inverseY - aimY );
+    ctx.stroke();
+    ctx.closePath();
+  }
+};
+
+const drawPlayerSprite = ( ctx, { position }) => {
+  const [ px, py ] = position.value;
   const inverseY = window.innerHeight - py; // invert Y so my math is easy
 
-  ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-  ctx.scale(1, 1);
   ctx.beginPath();
   ctx.arc( px, inverseY, 20, 0, Math.PI * 2);
   ctx.stroke();
   ctx.closePath();
+};
+
+const renderPlayer = ( ctx, player ) => {
+  drawAimVector( ctx, player );
+  drawPlayerSprite( ctx, player );
 };
 
 export default curry( renderPlayer );
